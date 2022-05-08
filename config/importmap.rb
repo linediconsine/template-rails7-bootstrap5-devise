42f51021ecb2268d11b4078226bc91e6b28ee6b3
Marco Amato <linediconsine@gmail.com>
2022-05-08 22:01:05 +0200

    update login

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,8 +1,8 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
-#pin "popper", to: 'popper.js', preload: true
-#pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.1.3/dist/js/bootstrap.esm.js"
-#pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.5/lib/index.js"
 pin "popper", to: 'popper.js', preload: true
 pin "bootstrap", to: 'bootstrap.min.js', preload: true
+pin "@hotwired/turbo-rails", to: "https://ga.jspm.io/npm:@hotwired/turbo-rails@7.1.1/app/javascript/turbo/index.js"
+pin "@hotwired/turbo", to: "https://ga.jspm.io/npm:@hotwired/turbo@7.1.0/dist/turbo.es2017-esm.js"
+pin "@rails/actioncable/src", to: "https://ga.jspm.io/npm:@rails/actioncable@7.0.2/src/index.js"
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   root to: "home#index"
   # Defines the root path route ("/")
   # root "articles#index"
+  #
+  #
+
 end
